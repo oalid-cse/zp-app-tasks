d5506b9be68f1e4eefd04ac62e14cae7b3ebf096
Abu Bin Oalid <oalid.mmj@gmail.com>
2024-09-24 18:22:03 +0600

    initital templte done

--- index.php
+++ index.php
@@ -69,6 +69,89 @@ ini_set('display_errors', '1');
         </div>
     </div>
 
+    <div class="card mt-4 createFontGroupCard" id="createFontGroupCard">
+        <div class="card-header">
+            <h5 class="card-title">
+                Create Font Group
+            </h5>
+            <div class="tools">
+                <button type="button" class="btn back-to-create d-none" onclick="backToCreateFontGroup()">Back To Create</button>
+            </div>
+        </div>
+        <div class="card-body">
+            <form id="fontGroupForm">
+                <div class="mb-3">
+                    <label for="groupName" class="form-label">Group Name</label>
+                    <input type="text" class="form-control" id="groupName" name="name" required>
+                </div>
+                <div class="font-group-item-wrapper">
+                    <div class="font-group-item">
+                        <div class="form-group font-list">
+                            <label class="form-label">Select Fonts</label>
+                            <select class="form-select fontSelect" name="font_ids[]" required
+                                    onchange="selectGroupFont(this)"></select>
+                        </div>
+                        <div class="form-group font-name">
+                            <label class="form-label">Font Name</label>
+                            <input type="text" class="form-control font-name" disabled>
+                        </div>
+                        <div class="action">
+                            <button type="button" class="font-group-item-delete" onclick="deleteGroupItem(this)">
+                                X
+                            </button>
+                        </div>
+                    </div>
+                    <div class="font-group-item">
+                        <div class="form-group font-list">
+                            <label class="form-label">Select Fonts</label>
+                            <select class="form-select fontSelect" name="font_ids[]" required
+                                    onchange="selectGroupFont(this)"></select>
+                        </div>
+                        <div class="form-group font-name">
+                            <label class="form-label">Font Name</label>
+                            <input type="text" class="form-control font-name" disabled>
+                        </div>
+                        <div class="action">
+                            <button type="button" class="font-group-item-delete" onclick="deleteGroupItem(this)">
+                                X
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="d-flex justify-content-between">
+                    <button type="button" onclick="addGroupItem()" class="btn btn-outline-success">Add Row</button>
+                    <button type="submit" class="btn btn-success fontGroupCreateBtn">Create</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <div class="card mt-4 mb-5">
+        <div class="card-header">
+            <h5 class="card-title">
+                Font Groups
+            </h5>
+        </div>
+        <div class="card-body">
+            <div class="table-wrapper">
+                <table class="table table-striped" id="fontGroupList">
+                    <thead>
+                    <tr>
+                        <th>NAME</th>
+                        <th>FONTS</th>
+                        <th class="text-center">COUNT</th>
+                        <th></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
